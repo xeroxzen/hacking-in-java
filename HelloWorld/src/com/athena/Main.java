a8dddcd3c54c9694e1abaec5bcd5b9daa3560e30
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-14 11:42:43 +0200

    Fixed point

--- HelloWorld/src/com/athena/Main.java
+++ HelloWorld/src/com/athena/Main.java
@@ -7,28 +7,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Variables
-        //int (4bytes), long(8 bytes), float(4 bytes), double(8 bytes), byte(1 byte), short (2bytes), char (2bytes), boolean (1byte);
+        /*
+        Variables
+        int (4bytes), long(8 bytes), float(4 bytes), double(8 bytes), byte(1 byte), short (2bytes), char (2bytes), boolean (1byte);
+        Primitive Types
+                byte myAge =22;
+                byte herAge= myAge;
+                long viewsCount= 3_234_678_453L;
+                float price =10.99F;
+                char letter = 'A';
+                boolean isEligible =false;
+        System.out.println(herAge);
+        Reference Types
+                Date now=new Date();
+                System.out.println(now);
+                byte x=1;
+                byte y=x;
+                x = 2;
+                System.out.println(y);
+        */
 
-        //Primitive Types
-//        byte myAge =22;
-//        byte herAge= myAge;
-//        long viewsCount= 3_234_678_453L;
-//        float price =10.99F;
-//        char letter = 'A';
-//        boolean isEligible =false;
-        //System.out.println(herAge);
 
-        //Reference Types
-//        Date now=new Date();
-//        System.out.println(now);
-
-//        byte x=1;
-//        byte y=x;
-//        x = 2;
-//        System.out.println(y);
-        Point point1 = new Point(x:1, y:1);
+        Point point1 = new Point(1, 1);
         Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point1);
+
+        //Strings
+        //String message = new String("Hello World");
 
     }
 }
