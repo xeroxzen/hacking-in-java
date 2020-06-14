3903629209443c0f9050d3f782a82cb8e375f675
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-15 00:20:13 +0200

    Running alright

--- MortgageCalculator/src/com/athena/Main.java
+++ MortgageCalculator/src/com/athena/Main.java
@@ -1,21 +1,31 @@
 package com.athena;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        double principal;
-//        double rate;
-//        int n;
-        double mortgage;
+        final byte MONTHS_IN_YEAR =12;
+        final byte PERCENT = 100;
 
-        Scanner principal = new Scanner(System.in);
-        Scanner rate = new Scanner(System.in);
-        Scanner n = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        mortgage = principal*rate(1+rate);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
 
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+        float monthlyInterest = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
