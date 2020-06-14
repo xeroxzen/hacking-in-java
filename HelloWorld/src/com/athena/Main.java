10df5710a30b0591ed07b2823c646a11e831e537
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-14 11:28:32 +0200

    There is sommething

--- HelloWorld/src/com/athena/Main.java
+++ HelloWorld/src/com/athena/Main.java
@@ -10,7 +10,7 @@ public class Main {
         //int (4bytes), long(8 bytes), float(4 bytes), double(8 bytes), byte(1 byte), short (2bytes), char (2bytes), boolean (1byte);
 
         //Primitive Types
-        byte myAge =22;
+//        byte myAge =22;
 //        byte herAge= myAge;
 //        long viewsCount= 3_234_678_453L;
 //        float price =10.99F;
@@ -19,8 +19,13 @@ public class Main {
         //System.out.println(herAge);
 
         //Reference Types
-        Date now=new Date();
-        System.out.println(now);
+//        Date now=new Date();
+//        System.out.println(now);
+
+        byte x=1;
+        byte y=x;
+        x = 2;
+        System.out.println(y);
 
     }
 }
