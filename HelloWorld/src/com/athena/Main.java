8cde47f72bda7d552ca54e8bcea86d54e74edf4a
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-14 11:25:34 +0200

    Primitive types

--- HelloWorld/src/com/athena/Main.java
+++ HelloWorld/src/com/athena/Main.java
@@ -1,5 +1,7 @@
 package com.athena;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,12 +9,18 @@ public class Main {
         //Variables
         //int (4bytes), long(8 bytes), float(4 bytes), double(8 bytes), byte(1 byte), short (2bytes), char (2bytes), boolean (1byte);
 
+        //Primitive Types
         byte myAge =22;
-        byte herAge= myAge;
-        long viewsCount= 3_234_678_453L;
-        float price =10.99F;
-        char letter = 'A';
-        boolean isEligible =false;
-        System.out.println(herAge);
+//        byte herAge= myAge;
+//        long viewsCount= 3_234_678_453L;
+//        float price =10.99F;
+//        char letter = 'A';
+//        boolean isEligible =false;
+        //System.out.println(herAge);
+
+        //Reference Types
+        Date now=new Date();
+        System.out.println(now);
+
     }
 }
