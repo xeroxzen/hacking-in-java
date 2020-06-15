55ede4ae38c158c1cdef835f8aeeb57fd8a4d927
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-15 13:28:10 +0200

    Loops

--- HelloWorld/src/com/athena/Main.java
+++ HelloWorld/src/com/athena/Main.java
@@ -145,9 +145,11 @@ Strings
         Scanner scanner = new Scanner(System.in);
         String input = "";
 //
-        while (!input.equals("quit")) {
+        while (true) {
             System.out.print("Input: ");
             input = scanner.next().toLowerCase();
+            if (input.equals("pass"))
+                continue;
             if (input.equals("quit"))
                 break;
             System.out.println(input);
