f9c76a8980697b88c497b829c3f320ae18228ab6
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-15 13:00:27 +0200

    FizzBuzz

---
+++ FizzBuzz/src/com/athena/Main.java
@@ -0,0 +1,21 @@
+package com.athena;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 3 == 0 && number % 5 == 0)
+            System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
+    }
+}
