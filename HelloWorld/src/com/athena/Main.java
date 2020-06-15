dc4ccfc2d09c9489650bb6162e70480d5f2fc80b
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-15 13:37:05 +0200

    foreach

--- HelloWorld/src/com/athena/Main.java
+++ HelloWorld/src/com/athena/Main.java
@@ -168,5 +168,8 @@ Strings
 
         for (int i =0; i < fruits.length; i++)
             System.out.println(fruits[i]);
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
     }
 }
